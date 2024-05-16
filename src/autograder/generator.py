5e257da989a67ea438aa577c6f4195cf76e761c8
Grayson Nocera <gnocera@purdue.edu>
2024-05-16 13:59:41 -0500

    first implementation of generator -> constant files working

--- src/autograder/generator.py
+++ src/autograder/generator.py
@@ -6,7 +6,7 @@ import pathlib
 class Generator:
     def __init__(self, config):
         self.config = config
-        self.zip = zipfile.ZipFile(f"{self.config["executable"]}.zip", "w")
+        self.zip = zipfile.ZipFile(f"{self.config["global"]["executable"]}.zip", "w")
 
     def generate(self):        
         self._generate_template_file("setup-sh", "setup.sh")
@@ -16,23 +16,24 @@ class Generator:
         def inject(file):
             with open(file, "w") as f:
                 f.write("#!/usr/bin/env bash\n")
-                for file in self.config["files_from_student"]:
+                for file in self.config["global"]["files_from_student"]:
                     f.write(f"cp /autograder/submission/{file} /autograder/source/{file}\n")
                 f.write("cd /autograder/source\n")
                 f.write("python3 run_tests.py\n")
         self._generate_template_file("run_autograder", "run_autograder", inject)
 
+        self.zip.close()
+
     def _generate_template_file(self, key, default_filename, inject=None):
         config = self.config[key] if key in self.config else {}
-        if not config:
-            path_to_template = pathlib.Path(__file__).parent / "templates" / default_filename
-            if inject:
-                inject(path_to_template)
-            self.zip.write(path_to_template, default_filename)
-            return
         if "injected" in config and config["injected"]:
             self.zip.write(config["path"], default_filename)
             return
+        path_to_template = pathlib.Path(__file__).parent / "templates" / default_filename
+        if inject:
+            inject(path_to_template)
+        self.zip.write(path_to_template, default_filename)
+        return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate an autograder")
