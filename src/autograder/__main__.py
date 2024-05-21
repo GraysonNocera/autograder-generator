c7187f092d6809046c69ded8c0648c1981103af8
Grayson Nocera <gnocera@purdue.edu>
2024-05-21 00:31:17 -0500

    move cli command to dedicated file

---
+++ src/autograder/__main__.py
@@ -0,0 +1,13 @@
+import argparse
+from autograder.generator import Generator
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate an autograder")
+    parser.add_argument("config", help="Path to the configuration file")
+    args = parser.parse_args()
+
+    generator = Generator(args.config)
+    generator.generate()
+
+if __name__ == "__main__":
+    main()
