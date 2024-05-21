bd4086eeb55c3ac81294ff4d193e3d7a2488e5ff
Grayson Nocera <gnocera@purdue.edu>
2024-05-21 01:16:08 -0500

    beginning of testing suite

---
+++ tests/test_demo.py
@@ -0,0 +1,20 @@
+from autograder.generator import Generator
+import pathlib
+import zipfile
+import os
+
+def test_demo():
+    assert True
+
+def test_writing_template_file():
+    path_to_config = pathlib.Path(__file__).parents[1] / "examples" / "hw18.toml"
+    generator = Generator(path_to_config)
+
+    config = {}
+    generator._generate_template_file(config, "test_files", "tests/test_files.py")
+    generator.zip.close()
+
+    with zipfile.ZipFile(f"{generator.config['executable']}.zip", "r") as zip:
+        assert zip.namelist() == ["tests/test_files.py"]
+
+    os.remove(f"{generator.config['executable']}.zip")
